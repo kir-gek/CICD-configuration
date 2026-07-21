ef39223bec87634e25e3c7cc465423bbde414717
pc3 <kirokronos@gmail.com>
2026-07-21 18:13:09 +0300

    try4

--- pets-front/src/App.tsx
+++ pets-front/src/App.tsx
@@ -16,7 +16,7 @@ const [message, setMessage] = useState('Загрузка...');
   }, []);
   return (
     <>
-     <h1>Новая версия</h1>
+     <h1>DDDDDDDDDDDDDDD версия</h1>
      <p>Get запрос на сервер:</p>
      <br></br>
      <p>{message}</p>

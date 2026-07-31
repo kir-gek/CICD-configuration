a9568d134e64e3f4a6f6ffaec877b5f2740aef12
kir-gek <kirokronos@gmail.com>
2026-07-31 23:05:31 +0300

    mini fix

--- pets-front/src/components/dogs/Dogs.tsx
+++ pets-front/src/components/dogs/Dogs.tsx
@@ -1,5 +1,11 @@
 import { useEffect, useState } from "react";
-import { createDog, deleteDog, fetchDogs, updateDog, type Dog } from "../../api/dogs";
+import {
+  createDog,
+  deleteDog,
+  fetchDogs,
+  updateDog,
+  type Dog,
+} from "../../api/dogs";
 import { OneDog } from "./OneDog";
 
 export const Dogs: React.FC = () => {
@@ -7,7 +13,7 @@ export const Dogs: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const [openCreator, setOpenCreator] = useState<boolean>(false);
- const [currentDescription, setCurrentDescription] = useState("");
+  const [currentDescription, setCurrentDescription] = useState("");
   const [currentTitle, setCurrentTitle] = useState("");
 
   const [confirm, setConfirm] = useState(false);
@@ -91,90 +97,85 @@ export const Dogs: React.FC = () => {
     );
   }
 
-  if (dogs.length === 0) {
-    return (
-      <div className="text-center py-8 font-mono text-sm text-slate-500">
-        [ Array is empty. No dogs found ]
-      </div>
-    );
-  }
-
   return (
     <div className="w-full">
       {/* Сетка карточек: 1 на мобилках, 2 на планшетах, 3 на десктопе */}
-      <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">{dogsAll}</div>
-      <button
-      className="bg-sky-100"
-        onClick={() => setOpenCreator(true)}>
-        Добавить породу
-      </button>
 
-      {openCreator && <div className="bg-sky-50">
-            <button
-              onClick={() => setOpenCreator(false)}
-              className="p-2 bg-white text-red-600 rounded-full hover:bg-red-50 transition-all shadow-md hover:shadow-lg"
-              title="Удалить"
-            >
-               ❌
-            </button>
-              <h2 className="text-xl font-bold mb-4">Создать породу</h2>
-      <form>
-        <input
-          type="text"
-          placeholder="Заголовок породы"
-          className="w-full p-2 border rounded-lg mb-2"
-          value={currentTitle}
-          onChange={(e) => setCurrentTitle(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Описание"
-          className="w-full p-2 border rounded-lg mb-2"
-          value={currentDescription}
-          onChange={(e) => setCurrentDescription(e.target.value)}
-        />
-
-        <button
-          className="w-full bg-green-600 text-white py-2 rounded-lg hover:bg-green-700 transition"
-          type="button"
-          onClick={() => setConfirm(true)}
-        >
-          Создать
-        </button>
-      </form>
- {confirm && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm">
-          <div className="bg-white p-6 rounded-2xl shadow-2xl w-96 text-center animate-fadeIn">
-            <h2 className="text-xl font-bold text-gray-800">Вы уверены?</h2>
-            <p className="text-gray-600 mt-2">Создать эту породу?</p>
-
-            <div className="flex justify-between mt-6">
-              <button
-                type="button"
-                className="flex-1 bg-emerald-600 text-white py-3 rounded-lg hover:bg-emerald-700 transition font-semibold shadow-lg mr-2"
-                onClick={() => {
-                  createNewDog();
-                  setConfirm(false);
-                }}
-              >
-                ✅ Да, изменить
-              </button>
-              <button
-                type="button"
-                className="flex-1 bg-gray-300 text-gray-800 py-3 rounded-lg hover:bg-gray-400 transition font-semibold shadow-lg ml-2"
-                onClick={() => setConfirm(false)}
-              >
-                ❌ Отмена
-              </button>
-            </div>
-          </div>
+      {dogs.length === 0 && (
+        <div className="text-center py-8 font-mono text-sm text-slate-500">
+          [ Array is empty. No dogs found ]
         </div>
       )}
 
+      <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">{dogsAll}</div>
+      <button className="bg-sky-100" onClick={() => setOpenCreator(true)}>
+        Добавить породу
+      </button>
 
-      </div>}
+      {openCreator && (
+        <div className="bg-sky-50">
+          <button
+            onClick={() => setOpenCreator(false)}
+            className="p-2 bg-white text-red-600 rounded-full hover:bg-red-50 transition-all shadow-md hover:shadow-lg"
+            title="Удалить"
+          >
+            ❌
+          </button>
+          <h2 className="text-xl font-bold mb-4">Создать породу</h2>
+          <form>
+            <input
+              type="text"
+              placeholder="Заголовок породы"
+              className="w-full p-2 border rounded-lg mb-2"
+              value={currentTitle}
+              onChange={(e) => setCurrentTitle(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Описание"
+              className="w-full p-2 border rounded-lg mb-2"
+              value={currentDescription}
+              onChange={(e) => setCurrentDescription(e.target.value)}
+            />
 
+            <button
+              className="w-full bg-green-600 text-white py-2 rounded-lg hover:bg-green-700 transition"
+              type="button"
+              onClick={() => setConfirm(true)}
+            >
+              Создать
+            </button>
+          </form>
+          {confirm && (
+            <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm">
+              <div className="bg-white p-6 rounded-2xl shadow-2xl w-96 text-center animate-fadeIn">
+                <h2 className="text-xl font-bold text-gray-800">Вы уверены?</h2>
+                <p className="text-gray-600 mt-2">Создать эту породу?</p>
 
+                <div className="flex justify-between mt-6">
+                  <button
+                    type="button"
+                    className="flex-1 bg-emerald-600 text-white py-3 rounded-lg hover:bg-emerald-700 transition font-semibold shadow-lg mr-2"
+                    onClick={() => {
+                      createNewDog();
+                      setConfirm(false);
+                    }}
+                  >
+                    ✅ Да, изменить
+                  </button>
+                  <button
+                    type="button"
+                    className="flex-1 bg-gray-300 text-gray-800 py-3 rounded-lg hover:bg-gray-400 transition font-semibold shadow-lg ml-2"
+                    onClick={() => setConfirm(false)}
+                  >
+                    ❌ Отмена
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };

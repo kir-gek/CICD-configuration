68fad6dd1bc03d43013155b59a85f41464f86508
k.gekman <kirokronos@gmail.com>
2026-08-04 21:24:06 +0300

    tesdt

--- pets-front/src/App.tsx
+++ pets-front/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div className="max-w-6xl mx-auto my-6 overflow-hidden rounded-xl border border-slate-800 bg-slate-950 p-6 shadow-xl">
       <div className="mb-6 flex items-center justify-between border-b border-slate-800 pb-3">
         <span className="font-mono text-xs font-bold uppercase tracking-wider text-indigo-400">
-           Список пород тестим!!!:
+           Список пород:
         </span>
       </div>
       <Dogs />

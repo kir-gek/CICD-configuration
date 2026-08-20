e3005f27abaf3a9b7aaae2904c50689f7a2a4f71
pc3 <kirokronos@gmail.com>
2026-08-20 10:41:30 +0300

    test2

--- pets-front/src/App.tsx
+++ pets-front/src/App.tsx
@@ -11,6 +11,9 @@ function App() {
         <span className="font-mono text-xs font-bold uppercase tracking-wider text-indigo-400">
            Тест 1(фронт онли начало ветки):
         </span>
+         <span className="font-mono text-xs font-bold uppercase tracking-wider text-indigo-400">
+           Тест 2(фронт онли продолжение ветки (сохранность данных)):
+        </span>
       </div>
       <Dogs />
     </div>

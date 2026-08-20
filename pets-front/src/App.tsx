e0a25c887fb3f9e896979c55d327a1b6f856339b
pc3 <kirokronos@gmail.com>
2026-08-20 10:41:23 +0300

    test2

--- pets-front/src/App.tsx
+++ pets-front/src/App.tsx
@@ -11,7 +11,6 @@ function App() {
         <span className="font-mono text-xs font-bold uppercase tracking-wider text-indigo-400">
            Тест 1(фронт онли начало ветки):
         </span>
-        <p>Делаем тест для проверки копирования БД</p>
       </div>
       <Dogs />
     </div>
